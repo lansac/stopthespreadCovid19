19fa5aa15b983555f7c598b329a8222493adf1e0
Satish <163satish@gmail.com>
2020-03-22 13:18:51 -0700

    Srirams changes

---
+++ src/main/java/com/covoid/tracker/covidtracker/dao/DeviceHistoryRepository.java
@@ -0,0 +1,14 @@
+package com.covoid.tracker.covidtracker.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.covoid.tracker.covidtracker.entity.DeviceHistory;
+
+public interface DeviceHistoryRepository extends MongoRepository<DeviceHistory, String> {
+
+	public Optional<List<DeviceHistory>> findByMacId(String macId);
+
+}
